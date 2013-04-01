117d7325e0723cae57c7b0f458fc718abc8acc1e
Franck Cuny <franck@lumberjaph.net>
2013-04-01 15:55:40 -0700

    Add a redirect function to the response type

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -10,6 +10,10 @@ type User struct {
 	Name string
 }
 
+func GetRedirect(w *rest.ResponseWriter, req *rest.Request) {
+	w.Redirect("/users/1", 302)
+}
+
 func GetUser(w *rest.ResponseWriter, req *rest.Request) {
 	user := User{
 		Id:   req.PathParam("id"),
@@ -21,6 +25,7 @@ func GetUser(w *rest.ResponseWriter, req *rest.Request) {
 func main() {
 	handler := rest.ResourceHandler{}
 	handler.SetRoutes(
+		rest.Route{"GET", "/utilisateurs/:id", GetRedirect},
 		rest.Route{"GET", "/users/:id", GetUser},
 	)
 	http.ListenAndServe(":8080", &handler)
