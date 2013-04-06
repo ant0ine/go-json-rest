98ee9e7b34592144c1dbb9139041299ef2cc7d13
Franck Cuny <franck@lumberjaph.net>
2013-04-06 14:44:53 -0700

    Add a few convenient functions for URL to the Request type.
    The three added functions allow an easier creation of URLs.  This can be
    useful when, in the application, the developer need to generate an
    absolute URL (e.g.: for a redirect, to link to a resource, etc).

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -10,6 +10,10 @@ type User struct {
 	Name string
 }
 
+func GetOldAPIUser(w *rest.ResponseWriter, req *rest.Request) {
+	http.Redirect(w, req.Request, req.UriFor("/users/1"), 302)
+}
+
 func GetUser(w *rest.ResponseWriter, req *rest.Request) {
 	user := User{
 		Id:   req.PathParam("id"),
@@ -21,6 +25,7 @@ func GetUser(w *rest.ResponseWriter, req *rest.Request) {
 func main() {
 	handler := rest.ResourceHandler{}
 	handler.SetRoutes(
+		rest.Route{"GET", "/user/:id", GetOldAPIUser},
 		rest.Route{"GET", "/users/:id", GetUser},
 	)
 	http.ListenAndServe(":8080", &handler)
--- request.go
+++ request.go
@@ -2,8 +2,11 @@ package rest
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"regexp"
 )
 
 // Inherit from http.Request, and provide additional methods.
@@ -30,4 +33,37 @@ func (self *Request) DecodeJsonPayload(v interface{}) error {
 		return err
 	}
 	return nil
+}
+
+// Returns an absolute URI for the base (scheme + host) of the application,
+// without the trailing slash.
+func (self *Request) UriBase() string {
+	scheme := self.URL.Scheme
+	if scheme == "" {
+		scheme = "http"
+	}
+
+	url := fmt.Sprintf("%s://%s", scheme, self.Host)
+
+	trailingSlash, _ := regexp.Compile("/$")
+	if trailingSlash.MatchString(url) == true {
+		url = trailingSlash.ReplaceAllString(url, "")
+	}
+	return url
+}
+
+// Returns an URI from the base and path.
+func (self *Request) UriFor(path string) string {
+	url := fmt.Sprintf("%s%s", self.UriBase(), path)
+	return url
+}
+
+// Returns an URI from the base, the path, and the parameters.
+func (self *Request) UriForWithParams(path string, parameters map[string]string) string {
+	query := url.Values{}
+	for k, v := range parameters {
+		query.Add(k, v)
+	}
+	url := fmt.Sprintf("%s?%s", self.UriFor(path), query.Encode())
+	return url
 }
