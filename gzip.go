300af0d31833f19ef1bbf569eaddc643b10df2b5
antoine <antoine.imbert@gmail.com>
2014-02-09 00:13:25 +0000

    Rewrite the gzip wrapper to always set Vary: Accept-Encoding

--- gzip.go
+++ gzip.go
@@ -9,11 +9,19 @@ import (
 type gzipResponseWriter struct {
 	http.ResponseWriter
 	wroteHeader bool
+	canGzip     bool
 }
 
 func (self *gzipResponseWriter) WriteHeader(code int) {
-	self.Header().Set("Content-Encoding", "gzip")
+
+	// always set the Vary header, even if this particular request
+	// is not gzipped.
 	self.Header().Add("Vary", "Accept-Encoding")
+
+	if self.canGzip {
+		self.Header().Set("Content-Encoding", "gzip")
+	}
+
 	self.ResponseWriter.WriteHeader(code)
 	self.wroteHeader = true
 }
@@ -24,22 +32,27 @@ func (self *gzipResponseWriter) Write(b []byte) (int, error) {
 		self.WriteHeader(http.StatusOK)
 	}
 
-	gzipWriter := gzip.NewWriter(self.ResponseWriter)
-	defer gzipWriter.Close()
-	return gzipWriter.Write(b)
+	if self.canGzip {
+		gzipWriter := gzip.NewWriter(self.ResponseWriter)
+		defer gzipWriter.Close()
+		return gzipWriter.Write(b)
+	} else {
+		return self.ResponseWriter.Write(b)
+	}
 }
 
 func (self *ResourceHandler) gzipWrapper(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		// determine if gzip is needed
-		if self.EnableGzip == true &&
-			strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			writer := &gzipResponseWriter{w, false}
-			// call the handler
+		if self.EnableGzip == true {
+			// gzip support enabled
+			canGzip := strings.Contains(r.Header.Get("Accept-Encoding"), "gzip")
+			// client accepts gzip ?
+			writer := &gzipResponseWriter{w, false, canGzip}
+			// call the handler with the wrapped writer
 			h(writer, r)
 		} else {
-			// call the handler
+			// gzip support disabled, don't do anything
 			h(w, r)
 		}
 	}
