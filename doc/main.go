d66849e31ee5003dd844d36e2bc608821f1d78ee
antoine <antoine.imbert@gmail.com>
2014-03-23 07:09:43 +0000

    go fmt

--- doc/main.go
+++ doc/main.go
@@ -1,52 +1,52 @@
 package main
 
-import(
-        "net/http"
-        "log"
-        "fmt"
-        "io/ioutil"
-        "flag"
-        "regexp"
-        "strings"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
 )
 
 func getPage(urlStr string) string {
-        res, err := http.Get(urlStr)
-        if err != nil {
-                log.Fatal(err)
-        }
-        body, err := ioutil.ReadAll(res.Body)
-        res.Body.Close()
-        if err != nil {
-                log.Fatal(err)
-        }
-        return string(body)
+	res, err := http.Get(urlStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(body)
 }
 
 func readTemplate(filename string) string {
-        fileBytes, err := ioutil.ReadFile(filename)
-        if err != nil {
-                log.Fatalf(err.Error())
-        }
-        return string(fileBytes)
+	fileBytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	return string(fileBytes)
 }
 
 func main() {
 
-        var tmplFilename = flag.String("in", "README.md.tmpl", "filename of the template")
-        tmplBody := readTemplate(*tmplFilename)
+	var tmplFilename = flag.String("in", "README.md.tmpl", "filename of the template")
+	tmplBody := readTemplate(*tmplFilename)
 
-        startTag := "<webInclude>"
-        endTag := "</webInclude>"
-        re := regexp.MustCompile(startTag + "[^<]*" + endTag)
-        for _, statement := range re.FindAllString(tmplBody, -1) {
+	startTag := "<webInclude>"
+	endTag := "</webInclude>"
+	re := regexp.MustCompile(startTag + "[^<]*" + endTag)
+	for _, statement := range re.FindAllString(tmplBody, -1) {
 
-                urlStr := strings.TrimPrefix(statement, startTag)
-                urlStr = strings.TrimSuffix(urlStr, endTag)
+		urlStr := strings.TrimPrefix(statement, startTag)
+		urlStr = strings.TrimSuffix(urlStr, endTag)
 
-                pageBody := getPage(urlStr)
+		pageBody := getPage(urlStr)
 
-                tmplBody = strings.Replace(tmplBody, statement, pageBody, -1)
-        }
-        fmt.Printf("%s", tmplBody)
+		tmplBody = strings.Replace(tmplBody, statement, pageBody, -1)
+	}
+	fmt.Printf("%s", tmplBody)
 }
