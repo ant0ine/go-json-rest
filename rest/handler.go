68e1c19b8e1dc533b99d08e79e95f5cf97e038a4
antoine <antoine.imbert@gmail.com>
2014-03-30 00:59:26 +0000

    New method Route.MakePath
    Allows reverse route resolution.

--- rest/handler.go
+++ rest/handler.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"mime"
 	"net/http"
-	"reflect"
 	"runtime/debug"
 	"strings"
 )
@@ -60,51 +59,6 @@ type ResourceHandler struct {
 
 	// Custom logger, optional, defaults to log.New(os.Stderr, "", log.LstdFlags)
 	Logger *log.Logger
-}
-
-// Route defines a route. It's used with SetRoutes.
-type Route struct {
-
-	// Any HTTP method. It will be used as uppercase to avoid common mistakes.
-	HttpMethod string
-
-	// A string like "/resource/:id.json".
-	// Placeholders supported are:
-	// :param that matches any char to the first '/' or '.'
-	// *splat that matches everything to the end of the string
-	// (placeholder names must be unique per PathExp)
-	PathExp string
-
-	// Code that will be executed when this route is taken.
-	Func HandlerFunc
-}
-
-// RouteObjectMethod creates a Route that points to an object method. It can be convenient to point to
-// an object method instead of a function, this helper makes it easy by passing the object instance and
-// the method name as parameters.
-func RouteObjectMethod(httpMethod string, pathExp string, objectInstance interface{}, objectMethod string) *Route {
-
-	value := reflect.ValueOf(objectInstance)
-	funcValue := value.MethodByName(objectMethod)
-	if funcValue.IsValid() == false {
-		panic(fmt.Sprintf(
-			"Cannot find the object method %s on %s",
-			objectMethod,
-			value,
-		))
-	}
-	routeFunc := func(w ResponseWriter, r *Request) {
-		funcValue.Call([]reflect.Value{
-			reflect.ValueOf(w),
-			reflect.ValueOf(r),
-		})
-	}
-
-	return &Route{
-		HttpMethod: httpMethod,
-		PathExp:    pathExp,
-		Func:       routeFunc,
-	}
 }
 
 // SetRoutes defines the Routes. The order the Routes matters,
---
+++ rest/route.go
@@ -0,0 +1,65 @@
+package rest
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// Route defines a route. It's used with SetRoutes.
+type Route struct {
+
+	// Any HTTP method. It will be used as uppercase to avoid common mistakes.
+	HttpMethod string
+
+	// A string like "/resource/:id.json".
+	// Placeholders supported are:
+	// :param that matches any char to the first '/' or '.'
+	// *splat that matches everything to the end of the string
+	// (placeholder names must be unique per PathExp)
+	PathExp string
+
+	// Code that will be executed when this route is taken.
+	Func HandlerFunc
+}
+
+// RouteObjectMethod creates a Route that points to an object method. It can be convenient to point to
+// an object method instead of a function, this helper makes it easy by passing the object instance and
+// the method name as parameters.
+func RouteObjectMethod(httpMethod string, pathExp string, objectInstance interface{}, objectMethod string) *Route {
+
+	value := reflect.ValueOf(objectInstance)
+	funcValue := value.MethodByName(objectMethod)
+	if funcValue.IsValid() == false {
+		panic(fmt.Sprintf(
+			"Cannot find the object method %s on %s",
+			objectMethod,
+			value,
+		))
+	}
+	routeFunc := func(w ResponseWriter, r *Request) {
+		funcValue.Call([]reflect.Value{
+			reflect.ValueOf(w),
+			reflect.ValueOf(r),
+		})
+	}
+
+	return &Route{
+		HttpMethod: httpMethod,
+		PathExp:    pathExp,
+		Func:       routeFunc,
+	}
+}
+
+// MakePath generates the path corresponding to this Route and the provided path parameters.
+// This is used for reverse route resolution.
+func (route *Route) MakePath(pathParams map[string]string) string {
+	path := route.PathExp
+	for paramName, paramValue := range pathParams {
+		paramPlaceholder := ":" + paramName
+		splatPlaceholder := "*" + paramName
+		r := strings.NewReplacer(paramPlaceholder, paramValue, splatPlaceholder, paramValue)
+		path = r.Replace(path)
+	}
+	return path
+}
