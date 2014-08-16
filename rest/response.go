a53004ece11a3ec546da332792de43a8bb5d9eb9
antoine <antoine.imbert@gmail.com>
2014-08-16 03:14:13 +0000

    Don't ignore the potential error on w.Write
    Fixes #83

--- rest/response.go
+++ rest/response.go
@@ -89,7 +89,10 @@ func (w *responseWriter) WriteJson(v interface{}) error {
 	if err != nil {
 		return err
 	}
-	w.Write(b)
+	_, err = w.Write(b)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
