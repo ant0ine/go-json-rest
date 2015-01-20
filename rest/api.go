17676db32ba1a6a635c1d6271bbe4c3ca8f60742
antoine <antoine.imbert@gmail.com>
2015-01-20 01:36:47 +0000

    Docstrings

--- rest/api.go
+++ rest/api.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 )
 
+// Api defines a stack of middlewares and an app.
 type Api struct {
         middlewares []Middleware
         app App
 }
 
+// NewApi makes a new Api object, the App is required.
 func NewApi(app App) *Api {
 
         if app == nil {
@@ -21,10 +23,14 @@ func NewApi(app App) *Api {
         }
 }
 
+// Use pushes one or multiple middlewares to the stack for middlewares
+// maintained in the Api object.
 func (api *Api) Use(middlewares ...Middleware) {
         api.middlewares = append(api.middlewares, middlewares...)
 }
 
+// MakeHandler wraps all the middlewares of the stack and the app together, and
+// returns an http.Handler ready to be used.
 func (api *Api) MakeHandler() http.Handler {
 	return http.HandlerFunc(
                 adapterFunc(
@@ -33,6 +39,7 @@ func (api *Api) MakeHandler() http.Handler {
         )
 }
 
+// Defines a stack of middlewares that is convenient for development.
 var DefaultDevStack = []Middleware{
         &accessLogApacheMiddleware{},
         &timerMiddleware{},
