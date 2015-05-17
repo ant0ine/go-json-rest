fc2c9a071db8a007b1d30d171b63af9b7366886c
antoine <antoine.imbert@gmail.com>
2015-05-17 19:22:26 +0000

    Update the documentation of the test pkg to not use ResourceHandler

--- rest/test/doc.go
+++ rest/test/doc.go
@@ -5,23 +5,29 @@
 // checks end up to be always the same. This test package tries to save
 // some typing by providing helpers for this particular use case.
 //
-//	package main
+//      package main
 //
-//	import (
-//		"github.com/ant0ine/go-json-rest/rest/test"
-//		"testing"
-//	)
+//      import (
+//              "github.com/ant0ine/go-json-rest/rest"
+//              "github.com/ant0ine/go-json-rest/rest/test"
+//              "testing"
+//      )
 //
-//	func TestSimpleRequest(t *testing.T) {
-//		handler := ResourceHandler{}
-//		handler.SetRoutes(
-//			Get("/r", func(w ResponseWriter, r *Request) {
-//				w.WriteJson(map[string]string{"Id": "123"})
-//			}),
-//		)
-//		recorded := test.RunRequest(t, &handler,
-//			test.MakeSimpleRequest("GET", "http://1.2.3.4/r", nil))
-//		recorded.CodeIs(200)
-//		recorded.ContentTypeIsJson()
-//	}
+//      func TestSimpleRequest(t *testing.T) {
+//              api := rest.NewApi()
+//              api.Use(rest.DefaultDevStack...)
+//              router, err := rest.MakeRouter(
+//                      rest.Get("/r", func(w rest.ResponseWriter, r *rest.Request) {
+//                              w.WriteJson(map[string]string{"Id": "123"})
+//                      }),
+//              )
+//              if err != nil {
+//                     log.Fatal(err)
+//              }
+//              api.SetApp(router)
+//              recorded := test.RunRequest(t, api.MakeHandler(),
+//                      test.MakeSimpleRequest("GET", "http://1.2.3.4/r", nil))
+//              recorded.CodeIs(200)
+//              recorded.ContentTypeIsJson()
+//      }
 package test
