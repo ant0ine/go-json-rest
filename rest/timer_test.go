9b4b10c0f1530511dda55af0c120aa1d006bdca5
fieldAbyss <j8cwsqe29e2p@gmail.com>
2021-01-24 03:40:54 +0900

    fix: change &start and &end to "start" and "end"

--- rest/timer_test.go
+++ rest/timer_test.go
@@ -19,7 +19,7 @@ func TestTimerMiddleware(t *testing.T) {
 			if r.Env["ELAPSED_TIME"] == nil {
 				t.Error("ELAPSED_TIME is nil")
 			}
-			elapsedTime := r.Env["ELAPSED_TIME"].(*time.Duration)
+			elapsedTime := r.Env["ELAPSED_TIME"].(time.Duration)
 			if elapsedTime.Nanoseconds() <= 0 {
 				t.Errorf(
 					"ELAPSED_TIME is expected to be at least 1 nanosecond %d",
@@ -30,7 +30,7 @@ func TestTimerMiddleware(t *testing.T) {
 			if r.Env["START_TIME"] == nil {
 				t.Error("START_TIME is nil")
 			}
-			start := r.Env["START_TIME"].(*time.Time)
+			start := r.Env["START_TIME"].(time.Time)
 			if start.After(time.Now()) {
 				t.Errorf(
 					"START_TIME is expected to be in the past %s",
